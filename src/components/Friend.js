248b6e429e17533a1493ac09f52c5c8e70780860
Aleksandr Fesenko <fesenkoas@gmail.com>
2023-01-01 11:13:57 +0200

    Update Friend.js

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from '../css_modules/borderRound.module.css';
 import { characters } from '../utils/constants';
-import { NavLink, useNavigate} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 const Friend = ({ friend, number }) => {
     const navigate = useNavigate();
